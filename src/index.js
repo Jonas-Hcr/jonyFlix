493e1be95baca4f05db130240ae6f6784eec0886
Jonas-hcr <programeicafe@outlook.com>
2020-08-01 10:37:18 -0300

    Paginas cadastro video e categoria

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Pagina404 = () => (<div>PÁGINA 404 - NOT FOUND</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route path="/" component={Home} exact />
+      <Route component={Pagina404} exact />
+
+    </Switch>
+
+  </BrowserRouter>,
   document.getElementById('root')
 );
